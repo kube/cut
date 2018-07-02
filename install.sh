4c037a54a5507a163b9ea7517d62f56dfe9fafe9
kube <chris@feijoo.fr>
2018-07-02 15:09:06 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ fi
 if [ "$1" == "remote" ];
 then
     # Install from Github repository
-    git clone --depth=1 git@github.com:kube/cut /usr/local/share
+    git clone --depth=1 https://github.com/kube/cut /usr/local/share
     ln -s /usr/local/share/cut/cutrun/cutrun.rb /usr/local/bin/cutrun
 else
     # Install from local clone
@@ -37,6 +37,7 @@ fi
 
 # Check that Cutrun was successfully installed
 if [ $(command -v cutrun) ];
+then
     echo "Cutrun successfully installed at $(cutrun --print-install-path)"
 else
     echo "Error during installation"
